b2bb9ed5422930142d8a7f0656782c181b0ac691
rjacob <rahnarjacob@gmail.com>
2024-07-13 16:25:35 +0530

    Added imports, middleware, connections, set up landing and 404 pages

---
+++ server.js
@@ -0,0 +1,55 @@
+//IMPORTS
+const dotenv = require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+const morgan = require("morgan")
+const session = require("express-session")
+const mongoStore = require("connect-mongo")
+const multer = require('multer') //part of image upload stretch
+
+//CONTROLLERS
+
+//CONSTANTS
+const app = express()
+const port = process.env.PORT ? process.env.PORT : "3000"
+const upload = multer({ dest: 'uploads/' }) //part of image upload stretch
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: mongoStore.create({
+            mongoUrl: process.env.MONGODB_URI
+        })
+    })
+)
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.get("*", (req, res) => {
+    res.render("404.ejs")
+})
+
+//SERVER CONNECTIONS
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB connected")
+        app.listen((process.env.PORT), () => {
+            console.log(`Port ${port} running`)
+        })
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+connect()
