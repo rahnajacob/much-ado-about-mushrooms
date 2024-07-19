dd1b62da7c6193434d731691f13d4377a25532ea
rjacob <rahnarjacob@gmail.com>
2024-07-19 16:50:20 +0530

    Corrected minor error in api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -65,10 +65,11 @@ const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
         console.log("MongoDB connected")
-        module.exports.handler = serverless(app);
     } catch (err) {
         console.log(err.message)
     }
 }
+
+module.exports.handler = serverless(app);
 
 connect()
